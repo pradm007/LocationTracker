aa37776695e9344120dc9b31a6e11c0472f09913
Pradeep Mahato <pradeepmahato007@gmail.com>
2016-05-07 14:59:50 +0530

    Moved to different package

---
+++ app/src/main/java/mm/locationtracker/activity/MainActivity.java
@@ -0,0 +1,45 @@
+package mm.locationtracker.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import mm.locationtracker.R;
+import mm.locationtracker.service.GPSTrackerService;
+
+public class MainActivity extends AppCompatActivity {
+
+    GPSTrackerService gpsTrackerService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button locationButton = (Button) findViewById(R.id.location_button);
+        locationButton.setOnClickListener(locationButtonOnClick);
+
+    }
+
+    View.OnClickListener locationButtonOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            gpsTrackerService = new GPSTrackerService(getApplicationContext());
+
+            if (gpsTrackerService.canGetLocation()) {
+                double latitude = gpsTrackerService.getLatitude();
+                double longitude = gpsTrackerService.getLongitude();
+
+                String locationStr = "Current location is \nLatitude : " + latitude + "\nLongitude : " + longitude;
+
+                Toast.makeText(getApplicationContext(), locationStr, Toast.LENGTH_SHORT).show();
+            } else {
+                gpsTrackerService.showSettingsAlert();
+                Toast.makeText(getApplicationContext(), "Cannot get location !! :(", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
