11333373bc1fc26347788c9d6a23e39bb60e6e0a
Pradeep Mahato <pradeepmahato007@gmail.com>
2016-05-07 23:27:00 +0530

    Added button for dumping location data.

--- app/src/main/java/mm/locationtracker/activity/MainActivity.java
+++ app/src/main/java/mm/locationtracker/activity/MainActivity.java
@@ -6,8 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 import mm.locationtracker.R;
+import mm.locationtracker.database.helper.DatableHandler;
+import mm.locationtracker.database.table.LocationTable;
 import mm.locationtracker.service.GPSTrackerService;
+import mm.locationtracker.utility.KMLFileCreator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button locationButton = (Button) findViewById(R.id.location_button);
         locationButton.setOnClickListener(locationButtonOnClick);
+
+        Button dumpKMLFile = (Button) findViewById(R.id.location_dump_kml_file);
+        dumpKMLFile.setOnClickListener(dumpKMLFileListener);
 
     }
 
@@ -39,6 +47,26 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 gpsTrackerService.showSettingsAlert();
                 Toast.makeText(getApplicationContext(), "Cannot get location !! :(", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    View.OnClickListener dumpKMLFileListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            DatableHandler datableHandler =  new DatableHandler(getApplicationContext(), "LOCATION_COORDINATES");
+            ArrayList<LocationTable> locationTableArrayList =  datableHandler.getAllLocation();
+
+            if (locationTableArrayList.isEmpty()) {
+                Toast.makeText(getApplicationContext(), "No record found in DB !! :(", Toast.LENGTH_SHORT).show();
+            } else {
+                boolean status = KMLFileCreator.createKMLFile(locationTableArrayList);
+
+                if (status) {
+                    Toast.makeText(getApplicationContext(), "File creation successfull", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "File creation failed", Toast.LENGTH_SHORT).show();
+                }
             }
         }
     };
