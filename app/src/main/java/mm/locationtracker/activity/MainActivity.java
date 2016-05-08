149b6f80a90bc62c40d546d90004498d709496cb
Pradeep Mahato <pradeepmahato007@gmail.com>
2016-05-08 12:03:23 +0530

    Added pin security

--- app/src/main/java/mm/locationtracker/activity/MainActivity.java
+++ app/src/main/java/mm/locationtracker/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package mm.locationtracker.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -12,6 +16,7 @@ import mm.locationtracker.R;
 import mm.locationtracker.database.helper.DatableHandler;
 import mm.locationtracker.database.table.LocationTable;
 import mm.locationtracker.service.GPSTrackerService;
+import mm.locationtracker.utility.ApplicationState;
 import mm.locationtracker.utility.CustomToast;
 import mm.locationtracker.utility.KMLFileCreator;
 
@@ -19,19 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
     GPSTrackerService gpsTrackerService;
 
+    Button locationButton;
+    Button dumpKMLFile;
+
+    LinearLayout pinContainer;
+    EditText pinEditTextView;
+    Button submitPin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         gpsTrackerService = new GPSTrackerService(getApplicationContext());
+        ApplicationState.setAccessLevel(ApplicationState.LEAST_ACCESS);
 
-        Button locationButton = (Button) findViewById(R.id.location_button);
+        locationButton = (Button) findViewById(R.id.location_button);
         locationButton.setOnClickListener(locationButtonOnClick);
 
-        Button dumpKMLFile = (Button) findViewById(R.id.location_dump_kml_file);
+        dumpKMLFile = (Button) findViewById(R.id.location_dump_kml_file);
         dumpKMLFile.setOnClickListener(dumpKMLFileListener);
 
+        pinContainer = (LinearLayout) findViewById(R.id.pinContainer);
+        pinEditTextView = (EditText) findViewById(R.id.pinTextView);
+        submitPin = (Button) findViewById(R.id.submitPin);
+
+        submitPin.setOnClickListener(submitPinClickListener);
     }
 
     View.OnClickListener locationButtonOnClick = new View.OnClickListener() {
@@ -71,4 +89,42 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     };
+
+    View.OnClickListener submitPinClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (pinEditTextView.getText().toString().equalsIgnoreCase(getResources().getString(R.string.correct_pin))) {
+                revealContents();
+            } else {
+                hideContents();
+            }
+        }
+    };
+
+    private void hideContents() {
+        pinContainer.setVisibility(View.GONE);
+        locationButton.setText(getResources().getString(R.string.get_current_location_hidden));
+        dumpKMLFile.setText(getResources().getString(R.string.create_dump_file_hidden));
+        locationButton.setVisibility(View.VISIBLE);
+        dumpKMLFile.setVisibility(View.VISIBLE);
+        ApplicationState.setAccessLevel(ApplicationState.LEAST_ACCESS);
+
+        hideKeyboard();
+    }
+
+    private void revealContents() {
+        pinContainer.setVisibility(View.GONE);
+        locationButton.setText(getResources().getString(R.string.get_current_location));
+        dumpKMLFile.setText(getResources().getString(R.string.create_dump_file));
+        locationButton.setVisibility(View.VISIBLE);
+        dumpKMLFile.setVisibility(View.VISIBLE);
+        ApplicationState.setAccessLevel(ApplicationState.FULL_ACCESS);
+
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
 }
