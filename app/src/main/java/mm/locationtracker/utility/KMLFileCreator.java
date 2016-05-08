4eed166e6a4cec92122794ccf38ebd32ec446000
Pradeep Mahato <pradeepmahato007@gmail.com>
2016-05-08 18:50:11 +0530

    Corrected the KML file genration to LineString

--- app/src/main/java/mm/locationtracker/utility/KMLFileCreator.java
+++ app/src/main/java/mm/locationtracker/utility/KMLFileCreator.java
@@ -23,29 +23,48 @@ public class KMLFileCreator {
 
         boolean status = true;
         String fileNameRevertBack = "";
+        String timeStamp = System.currentTimeMillis() + "";
 
         String kmlstart = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
                 "<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n";
 
-        String kmlelement = "", kmlelementStart = "\t<Placemark>\n" +
-                "\t<name>Simple placemark</name>\n" +
-                "\t<description>" + "location history" + "</description>\n" +
-                "\t<Point>\n" + "\t\t<coordinates>";
+        String kmlelement = "", kmlelementStart = "";
+        String kmlElementPrefix = "<Document>\n" +
+                "    <name>MM Locations</name>\n" +
+                "    <description>The cookie</description>\n" +
+                "    <Style id=\"yellowLineGreenPoly\">\n" +
+                "      <LineStyle>\n" +
+                "        <color>7f00ffff</color>\n" +
+                "        <width>4</width>\n" +
+                "      </LineStyle>\n" +
+                "      <PolyStyle>\n" +
+                "        <color>7f00ff00</color>\n" +
+                "      </PolyStyle>\n" +
+                "    </Style>";
+
         String locationCordinates = "";
 
-        for (int i = 0; i < locationTableArrayList.size(); i++) {
+        for (int i = 0; i < 20; i++) {
+
+            kmlelementStart = "\t<Placemark>\n" +
+                    "\t<name>" + i + "</name>\n" +
+                    "\t<description>" + "At " + CustomDate.getCurrentFormattedDate() + "</description>\n" +
+                    "\t<LineString>\n" + "\t\t<coordinates>";
+
             LocationTable locationTable = locationTableArrayList.get(i);
-            locationCordinates += locationTable.getLatitude() + "," + locationTable.getLongitude() + "," + 0;
+            locationCordinates = locationTable.getLatitude() + "," + locationTable.getLongitude() + "," + 0;
 
             if (i < locationTableArrayList.size() - 2) {
                 locationCordinates += " ";
             }
+
+            kmlelement += kmlelementStart + locationCordinates + "</coordinates>\n" +
+                    "\t</LineString>\n" +
+                    "\t</Placemark>\n";
+
         }
 
-        kmlelement = kmlelementStart + locationCordinates + "</coordinates>\n" +
-                "\t</Point>\n" +
-                "\t</Placemark>\n";
-
+        kmlelement = kmlElementPrefix + kmlelement + "\t</Document>\n";
         String kmlend = "</kml>";
 
         ArrayList<String> content = new ArrayList<String>();
@@ -55,7 +74,6 @@ public class KMLFileCreator {
 
         String kmltest = content.get(0) + content.get(1) + content.get(2);
 
-        String timeStamp = System.currentTimeMillis() + "";
         String fileName = filePath + filePrefix + timeStamp + ".kml";
         File testexists = new File(fileName);
 
