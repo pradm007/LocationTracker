775381fa65965ff583e4cd9b7f1982a03710fc3b
Pradeep Mahato <pradeepmahato007@gmail.com>
2016-05-08 17:32:53 +0530

    constants

---
+++ app/src/main/java/mm/locationtracker/utility/CustomConstants.java
@@ -0,0 +1,9 @@
+package mm.locationtracker.utility;
+
+/**
+ * Created by Pradeep Mahato 007 on 08-May-16.
+ */
+public class CustomConstants {
+
+    public static String SEND_TRACKING_MAIL = "send_tracking_mail";
+}
